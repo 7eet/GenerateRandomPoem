350b3d8d0197a14b75eb5b46ae2c9879f00b1679
7eet <raisimranjeetsingh@gmail.com>
2022-04-02 11:07:14 +0530

    updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <header className="text-light px-3 px-md-5 py-3">
-      <h1>Random Poem</h1>
+      <h1>Poem</h1>
     </header>
   );
 };
